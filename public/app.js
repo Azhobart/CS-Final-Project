063367790bdcef62905fe0216abfe7feceb4a0ba
SegaSonic-88 <gavin.talbot@utsuccess.org>
2024-07-08 12:59:32 -0600

    register user

--- public/app.js
+++ public/app.js
@@ -2,14 +2,45 @@ const URL = "http://localhost:8080";
 Vue.createApp({
   data() {
     return {
-      page: "login"
+      page: "login",
+
+      user: {
+        name: "",
+        username: "",
+        region: "",
+        authQuestion: "",
+        authAnswer: "",
+
+      },
+
     };
   },
   methods: {
     setPage: function(newPage) {
       this.page = newPage;
-      
-    }
+
+    },
+
+
+    registerUser: async function() {
+      let myHeaders = new Headers();
+      myHeaders.append("content-type", "application/json");
+
+      let requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: JSON.stringify(this.user),
+      };
+
+      let response = await fetch(`${URL}/users`, requestOptions);
+      if (response.status === 201) {
+        console.log("successfully registered");
+      } else {
+        console.log("failed to register")
+      }
+
+    },
+
 
   },
 
